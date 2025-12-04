bd1d204f07331f121255b37018eaf8aa99eb3fdd
SeungHunDev <sehn0x0@gmail.com>
2025-12-04 16:25:22 +0900

    Refactor:관리자 상품 이미지 업로드 및 삭제 로직 변경

--- shoppingmall-back/src/main/java/com/shoppingmallcoco/project/config/WebMvcConfig.java
+++ shoppingmall-back/src/main/java/com/shoppingmallcoco/project/config/WebMvcConfig.java
@@ -1,19 +1,19 @@
 package com.shoppingmallcoco.project.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    // 로컬 디스크에 저장된 이미지를 웹 브라우저에서 URL로 접근할 수 있게 매핑
+	
+	@Value("${file.upload-dir}") // 프로퍼티 값 주입
+    private String uploadDir;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // 브라우저 URL: http://localhost:8080/images/uploads/파일명.jpg
-        // 실제 저장 경로: C:/coco/uploads/파일명.jpg
-        
-        registry.addResourceHandler("/images/uploads/**") // 웹 접근 경로
-                .addResourceLocations("file:///C:/coco/uploads/"); // 로컬 파일 경로 (끝에 '/' 필수)
+    	// file:/// 접두사를 붙여서 경로 설정
+        registry.addResourceHandler("/images/**") // 웹 접근 경로
+        		.addResourceLocations("file:///" + uploadDir);
     }
 }
