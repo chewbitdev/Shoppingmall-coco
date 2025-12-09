2f2d45bd305f0d008919a4b9eb63cad34c56b340
SeungHunDev <sehn0x0@gmail.com>
2025-12-09 17:31:20 +0900

    refactor:불필요한 CORS 설정 제거

--- shoppingmall-back/src/main/java/com/shoppingmallcoco/project/config/WebMvcConfig.java
+++ shoppingmall-back/src/main/java/com/shoppingmallcoco/project/config/WebMvcConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
@@ -16,12 +15,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${file.upload-dir}") // 프로퍼티 값 주입
     private String uploadDir;
-    
-    private final String[] ALLOWED_ORIGINS = {
-            "http://13.231.28.89:3000",
-            "http://13.231.28.89",
-            "http://13.231.28.89:18080" 
-        };
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -52,14 +45,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
                     return new ClassPathResource("/static/index.html");
                 }
             });
-    }
-    
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins(ALLOWED_ORIGINS) // 허용할 프론트엔드 도메인
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true); // 인증 정보(쿠키, 토큰 등) 허용
     }
 }
