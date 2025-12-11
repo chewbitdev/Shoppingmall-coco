2b62078d1b6dcee043bd9f544e58cfdfec2bcacd
SeungHunDev <sehn0x0@gmail.com>
2025-12-11 14:26:44 +0900

    config:이미지 리소스에 대한 브라우저 캐싱 전략 적용

--- shoppingmall-back/src/main/java/com/shoppingmallcoco/project/config/WebMvcConfig.java
+++ shoppingmall-back/src/main/java/com/shoppingmallcoco/project/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.CacheControl;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -15,7 +17,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 업로드된 이미지 파일 서빙
         registry.addResourceHandler("/images/**")
-            .addResourceLocations("file:///" + uploadDir);
-
+            .addResourceLocations("file:///" + uploadDir)
+        	.setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS) // 1년간 캐싱
+        	.cachePublic() // 공용 네트워크(CDN 등)에서도 캐싱 허용
+        	.immutable()); // "이 파일은 기간 동안 절대 내용이 안 바뀜" 명시
     }
 }
