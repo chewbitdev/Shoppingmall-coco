f3c0c7ae59c4003d91c13cd5b2810be694703f0e
SeungHunDev <sehn0x0@gmail.com>
2025-12-12 10:07:09 +0900

    refactor:중복된 카테고리 아이콘 메뉴 영역 삭제

--- shoppingmall-front/src/pages/Home.js
+++ shoppingmall-front/src/pages/Home.js
@@ -1,72 +1,18 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
-import skincareImg from '../images/category/category_skincare.png';
-import makeupImg from '../images/category/category_makeup.png';
-import bodycareImg from '../images/category/category_bodycare.png'
-import hommeImg from '../images/category/category_homme.png';
 
 import '../css/Home.css';
 
 import SimpleSlider from './SimpleSlider'; // 메인배너 슬라이더 컴포넌트
 import BestSeller from './BestSeller'; // 인기상품 슬라이더 컴포넌트
-import Home_Comate from './Home_Comate';
+import HomeComate from './Home_Comate';
 
 const Home = () => {
-
-    const navigate = useNavigate();
-
-    const categoryMap = {
-        'skincare': 1,
-        'makeup': 2,
-        'bodycare': 3,
-        'homme': 4
-    };
-
-    // 카테고리 클릭 -> 상품 목록 페이지로 이동 (categoryNo 파라미터 전달)
-    const navigateToCategory = (categoryKey) => {
-        const categoryId = categoryMap[categoryKey];
-        
-        if (categoryId) {
-            // 상품 목록 페이지로 이동하면서 categoryNo를 쿼리 스트링으로 전달
-            navigate(`/product?categoryNo=${categoryId}`);
-        } else {
-            // 매핑된 ID가 없을 경우 전체 상품 페이지로 이동하거나 에러 처리
-            console.warn("알 수 없는 카테고리입니다.");
-            navigate('/product');
-        }
-    };
 
     return (
         <div>
             {/* 슬라이더 배너 */}
             <SimpleSlider />
             <div className="home_content">
-                {/* 카테고리 영역 */}
-                <div className="category_wrapper">
-                    <ul className="category_list">
-                        <li className="category_item">
-                            <div className="clickable_area" onClick={() => navigateToCategory('skincare')}>
-                            <img src={skincareImg} alt="category_img"/><div>SKIN CARE</div>
-                            </div>
-                        </li>
-                        <li className="category_item">
-                            <div className="clickable_area" onClick={() => navigateToCategory('makeup')}>
-                            <img src={makeupImg} alt="category_img"/><div>MAKE UP</div>
-                            </div>
-                        </li>
-                        <li className="category_item">
-                            <div className="clickable_area" onClick={() => navigateToCategory('bodycare')}>
-                            <img src={bodycareImg} alt="category_img"/><div>BODY CARE</div>
-                            </div>
-                        </li>
-                        <li className="category_item">
-                            <div className="clickable_area" onClick={() => navigateToCategory('homme')}>
-                            <img src={hommeImg} alt="category_img"/><div>HOMME</div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
                 {/* 인기 상품 영역 */}
                 <div className="popular_product_wrapper">
                     <div className="popular_title">
@@ -86,7 +32,7 @@ const Home = () => {
                         </div>
                     </div>
                     <div className="comate_list">
-                    <Home_Comate/>
+                    <HomeComate/>
                     </div>
                 </div>
             </div>
