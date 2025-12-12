59aba28048d670b7de0cdc1ac80138b4cd8743e4
Choo HyunMin <chu072409@gmail.com>
2025-12-12 09:54:41 +0900

    refactor:소나큐브 보안이슈 해결

--- shoppingmall-back/src/main/java/com/shoppingmallcoco/project/config/IamportConfig.java
+++ shoppingmall-back/src/main/java/com/shoppingmallcoco/project/config/IamportConfig.java
@@ -1,14 +1,18 @@
 package com.shoppingmallcoco.project.config;
 
 import com.siot.IamportRestClient.IamportClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class IamportConfig {
-    // 포트원 테스트용 키
-    private String apiKey = "2325122621874422"; 
-    private String apiSecret = "zqakFk09pez61EK3CgazLFgjPgE7HrRz9eq8LaLaZbIs7WiM1W45TPXFJ7K8t8slWGIear8AieA9r9t0"; 
+
+    @Value("${iamport.api-key}")
+    private String apiKey;
+
+    @Value("${iamport.api-secret}")
+    private String apiSecret;
 
     @Bean
     public IamportClient iamportClient() {
